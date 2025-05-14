d6fb308d4fe288a1e9e29eb3b435bff2d0f58e5a
Jess Boctor <contactme@jessboctor.com>
2025-05-14 11:16:05 -0500

    Clean up syntax

--- rm-debug-log.php
+++ rm-debug-log.php
@@ -28,40 +28,40 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 define( 'RM_DEBUG_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
 define( 'RM_DEBUG_LOG_PATH', RM_DEBUG_PLUGIN_PATH . '/logs/' );
 
-//See if there is a debug file directory in the uploads directory
-if( !file_exists( RM_DEBUG_LOG_PATH ) ){
+// See if there is a debug file directory in the uploads directory
+if ( !file_exists( RM_DEBUG_LOG_PATH ) ) {
 	//If is isn't there, create it
 	wp_mkdir_p( RM_DEBUG_LOG_PATH );
 }
 
-//Change the location of WordPress Native Debug log
-//Taken from https://wordpress.stackexchange.com/a/84171
+// Change the location of WordPress Native Debug log
+// Taken from https://wordpress.stackexchange.com/a/84171
 if ( defined('WP_DEBUG_LOG') && WP_DEBUG_LOG ) {
 	$date = date('m-d-Y');
 	ini_set( 'error_log', RM_DEBUG_LOG_PATH . 'rm-debug-' . $date . '.log' );
 }
 
-//Create the write_log() function
-if ( !function_exists( 'write_log' ) ){
-	function write_log( $log ){
+// Create the write_log() function
+if ( !function_exists( 'write_log' ) ) {
+	function write_log( $log ) {
 		
-		//Get the current date
+		// Get the current date
 		$date = date('m-d-Y');
 		$timestamp = date( '[ m-d-y H:i:s ] ' );
 		
-		//Get the file path
+		// Get the file path
 		$debug_file = RM_DEBUG_LOG_PATH . 'rm-debug-' . $date . '.log';
 		
-		//Check if the WP_DEBUG constant is set to true
+		// Check if the WP_DEBUG constant is set to true
         if ( true === WP_DEBUG ) {
 			
-			//If it is an array or object, print it to the error log
-            if( is_array( $log ) || is_object( $log ) ){
+			// If it is an array or object, print it to the error log
+            if ( is_array( $log ) || is_object( $log ) ) {
 				error_log( $timestamp, 3, $debug_file );
                 error_log( print_r( $log, true), 3, $debug_file );
 				error_log( PHP_EOL, 3, $debug_file );
             } else {
-				//register it to the error log
+				// Register it to the error log
                 error_log( $timestamp . $log . PHP_EOL, 3, $debug_file );
             }
         }
